09bd312a6ca280d3620feab93332bebafc00e42a
pencilwith-front <gunhosong@welliv.co.kr>
2021-04-01 09:24:42 +0900

    변경사항검토중

---
+++ lib/pages/temspage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TermsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SafeArea(
+        child: Center(
+          child: Text('Terms111111111 Page'),
+        ),
+      ),
+    );
+  }
+}
